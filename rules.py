091a0559a82d626afd6bae91112156e9892cc5be
prathmesh2507 <prathmeshbhoyar321@gmail.com>
2026-04-13 19:00:00 +0530

    project file updated

--- rules.py
+++ rules.py
@@ -10,13 +10,3 @@ def sort_by_date(file_path, date_format):
     return datetime.fromtimestamp(timestamp).strftime(date_format)
 
 
-def sort_by_size(file_path, size_categories):
-    size = file_path.stat().st_size
-
-    for category, limit in size_categories.items():
-        if size <= limit:
-            return category
-
-    return "Huge"
-
-#
